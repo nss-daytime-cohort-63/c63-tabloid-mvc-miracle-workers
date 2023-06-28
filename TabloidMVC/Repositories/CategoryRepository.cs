f54d1c7f6f4bee7e51390151c901a12aff2e637c
Nic <nic.lahde@gmail.com>
2023-06-28 10:02:21 -0500

    add category create + add authorize to index

--- TabloidMVC/Repositories/CategoryRepository.cs
+++ TabloidMVC/Repositories/CategoryRepository.cs
@@ -34,5 +34,23 @@ namespace TabloidMVC.Repositories
                 }
             }
         }
+
+        public void Add(Category category)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Category ([Name])
+                        OUTPUT INSERTED.ID
+                        VALUES (@Name)";
+                    cmd.Parameters.AddWithValue("@Name", category.Name);
+                   
+                    category.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
     }
 }
