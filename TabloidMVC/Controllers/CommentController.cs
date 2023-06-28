2df77fd38fd7283662d1e85c893ee4d3e3648dec
Garrett Crabb <garrettac01@gmail.com>
2023-06-28 15:01:30 -0500

    getting new main

---
+++ TabloidMVC/Controllers/CommentController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    [Authorize]
+    public class CommentController : Controller
+    {
+        private readonly ICommentRepository _commentRepository;
+
+        public CommentController(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        // GET: Comment
+        public ActionResult Index(int id)
+        {
+            var comments = _commentRepository.GetAllCommentsByPostId(id);
+            return View(comments);
+        }
+
+        // GET: Comment/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Comment/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comment/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Comment/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Comment/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Comment/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Comment/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
